5ead419c8f9e2c62751058a9017fc808a6164f3d
tstuderTOPIC <tstuder@topicdesign.com>
2014-07-23 15:54:38 -0400

    edited forms validated some issues, corrected.

--- app/models/count.rb
+++ app/models/count.rb
@@ -3,7 +3,10 @@ class Count < ActiveRecord::Base
   belongs_to :user
 
   def pct
-    goal / count
+     count / goal
   end
+
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
 end
